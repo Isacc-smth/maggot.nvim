b203b38b41be282815cf13300f2d5ee9d1e9e720
Isaac Feltes <isaacfg2812@gmail.com>
2025-10-29 22:04:09 -0300

    Renamed references to previous plugin

---
+++ colors/maggot.lua
@@ -0,0 +1,1 @@
+require("maggot").load()
---
+++ lua/maggot/palette.lua
@@ -0,0 +1,57 @@
+return {
+    -- sunset.nvim colors
+    red = "#DB5F50",
+    orange = "#e58e44",
+    green = "#77b886",
+    yellow = "#f7cb92", -- modded to be more of a peach color
+    blue = "#7689d9",
+    magenta = "#8f509d",
+    cyan = "#8fb9f4",
+    black = "#000000",
+    bright_red = "#ff6347",
+    bright_orange = "#f6aa6b",
+    bright_green = "#a3d6a5",
+    bright_yellow = "#ffe56e",
+    bright_blue = "#9eabe6",
+    bright_magenta = "#b78fc2",
+    bright_cyan = "#a4c4ff",
+    white = "#ffffff",
+    gutter_fg = "#78808f",
+    nontext = "#d2d6dc",
+
+    -- my colors (not all are used as of 07/07/25)
+
+    bg = "#080D04",
+    fg = "#F6E9B4",
+    cursorline = "#12190A",
+    selection = "#1B2310",
+    linenr = "#4C5033",
+    comment = "#757457",
+
+    -- Syntax
+    keyword = "#f28fad",
+    Function = "#89b4fa",
+    string = "#f9e2af",
+    constant = "#cdd6f4",
+    type = "#cba6f7",
+    number = "#fab387",
+    boolean = "#f38ba8",
+    operator = "#94a3b8",
+    variable = "#e0def4",
+
+    -- UI
+    cursor = "#f5a97f",
+    visual = "#292B1B",
+    search = "#f5c2e7",
+    statusline = "#1e293b",
+    menu_bg = "#1a2535",
+    menu_sel = "#3b4252",
+    fold = "#3e4a5a",
+    split = "#334155",
+
+    -- Diagnostics
+    diag_error = "#DB5F50",
+    diag_warn = "#FFB340",
+    diag_info = "#DDDE00",
+    diag_hint = "#B396C4",
+}
