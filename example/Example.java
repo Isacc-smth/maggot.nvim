63799011188af9d8eeaebcd9eaf03ab3d461a002
Isaac Feltes <isaacfg2812@gmail.com>
2025-11-18 23:44:04 -0300

    chore: Remove example folder

--- example/Example.java
+++
@@ -1,9 +0,0 @@
-// NOTE: Jdtls is enabled here
-public class Example {
-    public static Integer[] arr = new Integer[69];
-    private static final String MSG = "Feel free to puke at the code";
-
-    public static void main(String[] args) {
-        System.out.println(MSG);
-    }
-}
